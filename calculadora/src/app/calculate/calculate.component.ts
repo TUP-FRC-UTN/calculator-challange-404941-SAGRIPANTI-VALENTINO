e531d709f6fdfec54b6faa6a54ff4c6a525568a3
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-08-23 21:12:05 -0300

    componente hijo

---
+++ calculadora/src/app/calculate/calculate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculate.component.html',
+  styleUrl: './calculate.component.css'
+})
+export class CalculateComponent {
+
+}
