cdcbbd109e9530adc119bc56d31d0784add786d1
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-08-23 22:06:04 -0300

    lo ultimo

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -9,5 +9,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  @Output() eventoNro = new EventEmitter<number>()
+  @Output() eventoSigno = new EventEmitter<string>()
+  valor1 : number | undefined
+  valor2 : number | undefined
+  signo : string = ""
+
+  enviarDatos(){
+    this.eventoNro.emit(this.valor1)
+    this.eventoNro.emit(this.valor2)
+    this.eventoSigno.emit(this.signo)
+  }
 }
